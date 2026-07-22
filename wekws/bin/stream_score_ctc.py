3a5779ce4f5a9e65492a8d1e385328074b26602c
xumenglong <xumenglong.xml@alibaba-inc.com>
2026-07-23 01:03:19 +0800

    [fix] remove unused --token_file/--lexicon_file from stream_score_ctc.py

--- wekws/bin/stream_score_ctc.py
+++ wekws/bin/stream_score_ctc.py
@@ -71,14 +71,6 @@ def get_args():
                         type=str,
                         default=None,
                         help='the keywords, split with comma(,)')
-    parser.add_argument('--token_file',
-                        type=str,
-                        default=None,
-                        help='the path of tokens.txt')
-    parser.add_argument('--lexicon_file',
-                        type=str,
-                        default=None,
-                        help='the path of lexicon.txt')
     parser.add_argument('--score_beam_size',
                         default=3,
                         type=int,
